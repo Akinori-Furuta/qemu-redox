de685ba89dd114dd86663a56ee93e392a7ae1e2b
Akinori Furuta <afuruta@m7.dion.ne.jp>
2025-10-25 17:07:23 +0900

    Find latest Redox OS .iso or .iso.zst image
    * There is only .iso.zst image, decompress it.

--- start-redox-install.sh
+++ start-redox-install.sh
@@ -14,6 +14,47 @@ then
 	qemu-img create -f "${RedoxHddFormat}" "${RedoxHdd}" "${RedoxHddSize}"
 fi
 
+# arg: none
+# note: pipe-line process
+#  list files |
+#  duplicate file_name |
+#  extract date build_number file_name |
+#  sort {date, build_number} in reverse order |
+#  take first line |
+#  extract 3rd field
+function FindRedoxIso() {
+	push "${my_dir}" > /dev/null
+
+	ls redox_*.iso redox_*.iso.zst | \
+	sed 's/\(^.*$\)/\1 \1/' | \
+	sed 's/^[[:alpha:]]*_[[:alpha:]]*_.*_\([0-9]\+-[0-9]\+-[0-9]\+\)_\([0-9]\+\).*[[:space:]]\+\(.*\)/\1 \2 \3/' | \
+	sort -s -k 1,1r --key=2,2nr | \
+	head -1 | \
+	cut -d ' ' -f 3
+
+	popd > /dev/null
+}
+
+if [[ -z "${RedoxIso}" ]]
+then
+	RedoxIso="$( FindRedoxIso )"
+	if [[ -n "${RedoxIso}" ]]
+	then
+		redox_find="${my_dir}/${RedoxIso}"
+		redox_iso="${my_dir}/${RedoxIso%.zst}"
+
+		if [[ ! -e "${redox_iso}" ]]
+		then
+			if [[ ( "${RedoxIso}" == *.zst ) && ( -f "${redox_find}" ) ]]
+			then
+				echo "${redox_find}: Decompress zst image file."
+				unzstd "${redox_find}"
+			fi
+		fi
+		RedoxIso="${redox_iso}"
+	fi
+fi
+
 if [[ ! -e "${RedoxIso}" ]]
 then
 	echo "$0: Can not find Redox Desktop installer ISO image file."
